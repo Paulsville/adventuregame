42e04d0eff5664c2d91eca969dce4aa6c14c2d5d
Paul <pauldr@pbmail.co.uk>
2018-08-02 16:18:58 +0100

    2018-08-02

--- AdventureGame/Engine/Tile.cs
+++ AdventureGame/Engine/Tile.cs
@@ -9,5 +9,14 @@ namespace Engine
     public class Tile
     {
         int ID { get; set; }
+        string Name { get; set; }
+        bool CanWalk { get; set; }
+        
+        public Tile(int id, string name, bool canWalk)
+        {
+            ID = id;
+            Name = name;
+            CanWalk = canWalk;
+        }
     }
 }
